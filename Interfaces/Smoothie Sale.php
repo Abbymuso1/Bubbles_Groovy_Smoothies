0f8fa95beaf98709d149ab8b56fbc262abeb97c4
Abbymuso1 <bethkajuju@BK-HomeComputer.local>
2021-09-01 16:00:18 +0300

    first commit

---
+++ Interfaces/Smoothie Sale.php
@@ -0,0 +1,218 @@
+<!DOCTYPE html>
+<html lang="en">
+<header>
+    <meta charset="utf-8">
+    <title>Bubbles Groovy Smoothies</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta name="author" content="Abi Muso">
+    <meta name="description" content="Smoothie Online sale application">
+    <link rel="stylesheet" href="homepage.css">
+</header>
+
+<body class="body">
+
+    <div class="home">
+        <style>
+            .home {
+                height: 900px;
+                margin-right: -10px;
+                margin-top: -10px;
+                margin-left: -10px;
+                margin-bottom: -10px;
+                background-image: url("Css_Styling/Images/pineapple.jpeg");
+                position: relative;
+                background-repeat: no-repeat;
+                background-size: cover;
+                opacity: 1;
+                
+            }
+        </style>
+
+        <div class="header">
+
+
+            <div class="logo">
+                <style>
+                    .logo {
+                        background-image: url("Css_Styling/Images/logo.jpeg");
+                        background-color: pink;
+                        background-position: top;
+                        background-repeat: no-repeat;
+                        background-size: cover;
+                        border-style: none;
+                    }
+                </style>
+            </div>
+            <h1>Bubbles Groovy Smoothies</h1>
+
+
+            <a href="login.php">
+                <button id="button1">Log In</button>
+            </a>
+
+
+            <!-- <nav class="enticingimage"> -->
+            <nav class="Navbar">
+                <a href="#">Home</a>
+                <a href="#">Contact Us</a>
+                <a href="#">Smoothie Menu</a>
+                <a href="#">Service</a>
+
+            </nav>
+        </div>
+
+
+        <div class="welcome">
+
+            <!-- the headline -->
+            <h1>Smoothie time</h1>
+            <h3>Get your morning started with this quick<br> and easy to make healthy smoothie. </br></h3>
+
+            <!-- call to action -->
+            <button type="button"><a href="login.php"><span>Enjoy one now</span></a></button>
+
+        </div>
+
+        <div class="marque">
+            <marquee scrollamount="10" style="font-size: 200%;" style="font-family: 'Brush Script MT', 'Times New Roman';">🥰Enjoy a Bubbles Groovy Smoothie today!🥰</marquee>
+        </div>
+    </div>
+
+
+
+
+    <div class="content">
+        <h1>They come in different flavours:</h1>
+
+        <!-- photos -->
+        <table class="table1" border=1px cellspacing="2px">
+            <tr>
+                <th>Sexy StrawBerry </th>
+                <td>Image</td>
+                <th>Mangoey Mango </th>
+                <td>Image</td>
+                <th>Kunning Kiwi</th>
+                <td>Image</td>
+                <th>Pretty Pineappple </th>
+                <td>Image</td>
+            </tr>
+        </table>
+    </div>
+
+
+    <div class="content2">
+        <h1>Why Bubble's Smoothies?</h1>
+    </div>
+
+   
+        <div class="list">
+
+            <i>
+                <h4> 1. Energy-booster drinks</h4>
+            </i>
+
+            <i>
+                <h4>2. High in Vitamins and minerals</h4>
+            </i>
+
+            <i>
+                <h4>3. Amazing Complement</h4>
+            </i>
+
+            <i>
+                <h4> 4. Great taste</h4>
+            </i>
+
+        </div>
+
+
+
+        <fieldset>
+            <legend>
+                <div class="announcements">
+
+                    <h2> More amazing flavours are coming up!</h2>
+                </div>
+
+
+                <h1>Fun Melon</h1>
+
+
+                <h1 >Lebron lemon</h1>
+
+
+                <h1 class="announcementside">Sporange Orange</h1>
+
+            </legend>
+        </fieldset>
+
+
+        <div class="trust">
+            <h1>The Highlights</h1>
+        </div>
+
+
+        <div class="trustindicators">
+
+            <i>
+                <!-- textcontent -->
+                <p>1.What do we do?<br>We sell smoothies to all customers for very affordable prices.</p>
+            </i>
+
+
+            <i>
+                <p>2. Why us?<br>Certified 99.9% hygiene and made with a touch of love</p>
+            </i>
+        </div>
+
+
+
+
+        <div class="trust2">
+            <!-- social proof -->
+            <h1>Testimonials</h1>
+        </div>
+
+
+
+        <div class="trustindicators2">
+
+            <i>
+                <p>Hi Bubbles Groovy Smoothies,<br>Please do not close shop,<br>We are in love with your
+                    Smoothies!<br>~Jane Array</p>
+            </i>
+
+            <i>
+                <p>Dear Bubbles Groovy Smoothies,<br>I have had your smoothies for ten years now,<br> and the
+                    taste remains unique from the rest.<br>How do you do it?<br>~Peterson</p>
+            </i>
+        </div>
+
+
+        <!-- footer -->
+        <div class="hi">
+
+            <table>
+
+                <th>Contact us</th>
+                <li><a href="../contact_interface/contact.php">Email address</a></li>
+                <li> <a href="../contact_interface/contact.php">Phone Number</a></li>
+                <li><a href="../contact_interface/contact.php">Postal Address</a></li>
+
+
+
+
+                <ul>
+                    <h4>Offers</h4>
+                    <li><a href="Smoothie Sale.php">Buy two smoothies get one free!</a></li>
+                    <li><a href="Smoothie Sale.php">A new flavour of smoothie comes with cookie</a></li>
+                    <li><a href="../order_interface/menu.php">Smoothie Sale coming soon</a></li>
+                </ul>
+            </table>
+        </div>
+
+
+</body>
+
+</html>
